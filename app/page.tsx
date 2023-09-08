bc4c952f24ae356c273beb1c2fbd9ba1af0e1e40
Joel Rdz <joelrdzh@gmail.com>
2023-09-08 10:16:35 -0600

    add shadcn button to next

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className="grid place-items-center h-screen">
-      Next.js
+      <Button size="lg" variant="outline">Click Me!</Button>
     </main>
   );
 }
